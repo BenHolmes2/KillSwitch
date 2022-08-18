468040702981e274125fc0e3ae3efd2f5373119d
chefxdxd <tganatsos@gmail.com>
2022-08-19 06:35:27 +1000

    created FPS counter for perfomance testing

---
+++ Scripts/FPSCounter.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using TMPro;
+
+public class FPSCounter : MonoBehaviour
+{
+    public TextMeshProUGUI FPSText;
+
+    private float pollingTime = 1f;
+    private float time;
+    private int frameCount;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        frameCount++;
+        
+        if(time >= pollingTime)
+        {
+            int frameRate = Mathf.RoundToInt(frameCount / time);
+            FPSText.text = frameRate.ToString() + " FPS";
+
+            time -= pollingTime;
+            frameCount = 0;
+        }
+    }
+}
