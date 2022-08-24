0ef6b424bb1b767a77b25c380848c6dec9b79afe
chefxdxd <tganatsos@gmail.com>
2022-08-24 21:38:52 +1000

    finished bridge room

--- Scripts/BridgeMovement.cs
+++ Scripts/BridgeMovement.cs
@@ -4,25 +4,56 @@ using UnityEngine;
 
 public class BridgeMovement : MonoBehaviour
 {
-    public bool isStopped = false;
-    private Animator animator;
+    private Animator bridgeAnim;
+    private Animator gear1Anim;
+    private Animator gear2Anim;
+
+    public GameObject bridge;
+    public GameObject gear1;
+    public GameObject gear2;
+
+    private int counter = 0;
 
 
-    private void Awake()
+
+    void Awake()
     {
-        animator = GetComponent<Animator>();
-    }
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        bridgeAnim = bridge.GetComponent<Animator>();
+        gear1Anim = gear1.GetComponent<Animator>();
+        gear2Anim = gear2.GetComponent<Animator>();
+        counter = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (counter == 0 )
+        {
+            bridgeAnim.SetBool("On", true);
+            gear1Anim.SetBool("On", true);
+            gear2Anim.SetBool("On", true);
+        }
+        else
+        {
+            bridgeAnim.SetBool("On", false);
+            gear1Anim.SetBool("On", false);
+            gear2Anim.SetBool("On", false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        if (collision.gameObject.name == "QuickRigCharacter_Hips")
+        {
+            counter++;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.name == "QuickRigCharacter_Hips")
+        {
+            counter--;
+        }
     }
 }
