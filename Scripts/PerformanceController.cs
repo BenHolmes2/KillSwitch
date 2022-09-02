dfd28167a3522829cfc78328e158248427ba7695
chefxdxd <tganatsos@gmail.com>
2022-09-02 11:00:59 +1000

    Implementing performance controller

---
+++ Scripts/PerformanceController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+public class PerformanceController : MonoBehaviour
+{
+    private double roomCount;
+    private bool exitRoom;
+    private int bodyCountTotal;
+    private GameController controller;
+    private FPSCounter fCounter;
+    int[] bodyCountRooms = new int[10];
+    double[] roomTimes = new double[10];
+    int FPS;
+
+
+
+
+
+
+}
