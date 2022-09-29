a6bdb10e7d4ba392f62b4972c52b45fdbca81e8b
N0BLEFIVE <103296174@student.swin.edu.au>
2022-09-29 13:13:43 +1000

    Messing around with player character animations

--- Scripts/DeathCollider.cs
+++ Scripts/DeathCollider.cs
@@ -113,16 +113,16 @@ public class DeathCollider : MonoBehaviour
             }
 
             //turn this back on if we want the buzzsaw to destroy bodies with a blood effect
-            //if (other.gameObject.CompareTag("canPickUp") || other.gameObject.CompareTag("canPickUpDeath"))
-            //{
-            //    if (!Controller.isRespawn)
-            //    {
-            //        //bloodEffect.transform.position = other.transform.position;
-            //        Controller.spawnedPlayer.GetComponent<PlayerController>().ToggleCollisions(false);
-            //        Destroy(other.gameObject.transform.root.gameObject);
-            //        //Instantiate(bloodEffect);
-            //    }
-            //}
+            if (other.gameObject.CompareTag("canPickUp") || other.gameObject.CompareTag("canPickUpDeath"))
+            {
+                if (!Controller.isRespawn)
+                {
+                    //bloodEffect.transform.position = other.transform.position;
+                    Controller.spawnedPlayer.GetComponent<PlayerController>().ToggleCollisions(false);
+                    Destroy(other.gameObject.transform.root.gameObject);
+                    //Instantiate(bloodEffect);
+                }
+            }
         }
     }
 
