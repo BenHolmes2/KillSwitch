e97b34716e553b56bdb07c7ef6a8e90fab6f5957
N0BLEFIVE <103296174@student.swin.edu.au>
2022-09-30 16:06:58 +1000

    fiddling with doors

--- Scripts/DoorMovement.cs
+++ Scripts/DoorMovement.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DoorMovement : MonoBehaviour, IDoor
 {
 
@@ -11,43 +12,66 @@ public class DoorMovement : MonoBehaviour, IDoor
     public GameObject Door_R;
     private Renderer Light_L;
     private Renderer Light_R;
-    public bool dnew=false;
+    public bool dnew = false;
+    [ColorUsage(true, true)]
+    private Color red = Color.red;
+    [ColorUsage(true, true)]
+    private Color green = Color.green;
 
     private void Awake()
     {
         animator = GetComponent<Animator>();
-        if (dnew) 
+        if (dnew)
         {
             Light_L = Door_L.GetComponent<Renderer>();
             Light_R = Door_R.GetComponent<Renderer>();
-            Light_L.material.EnableKeyword("_EMISSION");
-            Light_R.material.EnableKeyword("_EMISSION");
-            Light_L.material.SetColor("_EmissionColor", Color.red);
-            Light_R.material.SetColor("_EmissionColor", Color.red); 
+            Light_L.material.EnableKeyword("_EmissionColor");
+            Light_R.material.EnableKeyword("_EmissionColor");
+            Light_L.material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+            Light_R.material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+            Light_L.material.SetColor("_EmissionColor", red);
+            Light_R.material.SetColor("_EmissionColor", red);
+            RendererExtensions.UpdateGIMaterials(Light_L);
+            RendererExtensions.UpdateGIMaterials(Light_R);
+            DynamicGI.SetEmissive(Light_L, Color.red * 100000);
+            DynamicGI.SetEmissive(Light_R, Color.red * 100000);
+            DynamicGI.UpdateEnvironment();
         }
     }
 
     public void OpenDoor()
     {
-        if (dnew) 
+        if (dnew)
         {
-            Light_L.material.EnableKeyword("_EMISSION");
-            Light_R.material.EnableKeyword("_EMISSION");
-            Light_L.material.SetColor("_EmissionColor", Color.green);
-            Light_R.material.SetColor("_EmissionColor", Color.green);
+
+            Light_L.material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+            Light_R.material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+            Light_L.material.SetColor("_EmissionColor", green);
+            Light_R.material.SetColor("_EmissionColor", green);
+            RendererExtensions.UpdateGIMaterials(Light_L);
+            RendererExtensions.UpdateGIMaterials(Light_R);
+            DynamicGI.SetEmissive(Light_L, Color.green * 100000);
+            DynamicGI.SetEmissive(Light_R, Color.green * 100000);
+            DynamicGI.UpdateEnvironment();
         }
         animator.SetBool("Open", true);
     }
 
     public void CloseDoor()
-    {   
+    {
         animator.SetBool("Open", false);
         if (dnew)
         {
-            Light_L.material.EnableKeyword("_EMISSION");
-            Light_R.material.EnableKeyword("_EMISSION");
-            Light_L.material.SetColor("_EmissionColor", Color.red);
-            Light_R.material.SetColor("_EmissionColor", Color.red);
+
+            Light_L.material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+            Light_R.material.globalIlluminationFlags = MaterialGlobalIlluminationFlags.RealtimeEmissive;
+            Light_L.material.SetColor("_EmissionColor", red);
+            Light_R.material.SetColor("_EmissionColor", red);
+            RendererExtensions.UpdateGIMaterials(Light_L);
+            RendererExtensions.UpdateGIMaterials(Light_R);
+            DynamicGI.SetEmissive(Light_L, Color.red * 100000);
+            DynamicGI.SetEmissive(Light_R, Color.red * 100000);
+            DynamicGI.UpdateEnvironment();
         }
     }
 
